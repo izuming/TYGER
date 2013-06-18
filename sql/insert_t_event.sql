6b33c8cdaff939befd7f487593b1712fc3d46c73
izumiya <masato.izumiya@gmail.com>
2013-06-18 19:54:00 +0900

    確認用データの登録用SQL

---
+++ sql/insert_t_event.sql
@@ -0,0 +1,2 @@
+insert into t_event(event_id, title, subtitle, sponsor_name, event_detail, place, url, open_start_time, open_end_time, acceptance_start_time, acceptance_end_time, stop_flg, delete_flg, updt_time)
+values(1,'bbb','ccc','ddd','eee', 'fff','http://google.com','2013-06-18 12:00:00.0','2013-06-18 12:00:00.0','2013-06-18 12:00:00.0','2013-06-18 12:00:00.0',false,false,'2013-06-18 12:00:00.0')
